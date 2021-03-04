14779c2995c1517a247ab5b613f0780d4a73e60c
BabyBoChen <35010886+BabyBoChen@users.noreply.github.com>
2021-03-04 19:12:51 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleMine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("0,0");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("1,0");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("2,0");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("3,0");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("4,0");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("0,1");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("1,1");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("2,1");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("3,1");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("4,1");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("0,2");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("1,2");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("2,2");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("3,2");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("4,2");
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("0,3");
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("1,3");
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("2,3");
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("3,3");
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("4,3");
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("0,4");
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("1,4");
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("2,4");
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("3,4");
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("4,4");
+        }
+    }
+}
